8a3395bb6e03e06d6e0cff7203330d6f53d30b52
Emmett6401 <46923369+Emmett6401@users.noreply.github.com>
2024-04-26 05:00:43 +0000

    많이 바꿨다고

--- Home.py
+++ Home.py
@@ -3,57 +3,47 @@ import streamlit as st
 from PIL import Image
 
 # Confit
-st.set_page_config(page_title='Cross Chain Monitoring Tool', page_icon=':bar_chart:', layout='wide')
+st.set_page_config(page_title='우송대학교 IT 센터', page_icon=':bar_chart:', layout='wide')
 
 # Outlier warning
-st.warning("""
-    The data within this app is no longer being updated. Feel free to check out the same author's
-    new tool called the [Outlier](https://outlier.streamlit.app/) for a more modern UI/UX in
-    presenting the same set of data.
-""")
+# st.warning("""
+#     이 프로그램은 [Outlier](https://outlier.streamlit.app/)를 가져와서 만든것이고 더 많은 modern UI/UX in
+#     presenting the same set of data.
+# """)
 
 # Title
-st.title('Cross Chain Monitoring Tool')
-
+st.title('늘 새로움을 만들다')
+st.divider()
 # Blockchains
 c1, c2, c3, c4, c5, c6 = st.columns(6)
 c1.image(Image.open('images/arbitrum-logo.png'))
-c2.image(Image.open('images/avalanche-logo.png'))
-c3.image(Image.open('images/bsc-logo.png'))
+c2.image(Image.open('images/1wsu1.png'))
+c3.image(Image.open('images/nostalgia.jpg'))
 c4.image(Image.open('images/ethereum-logo.png'))
 c5.image(Image.open('images/optimism-logo.png'))
 c6.image(Image.open('images/polygon-logo.png'))
 
 # Introduction
+st.subheader('커리큘럼')
 st.write(
     """
-    The crypto industry continues to progress and its development has never stopped. Contributors
-    of each blockchain keep developing each segment of the industry and the whole crypto ecosystem.
-    This tool is designed to allow viewers to journey into the world of crypto ecosystems of some
-    of the major blockchains, and compare their performance.
-
-    This tool is designed and structured in multiple **Pages** that are accessible using the sidebar.
-    Each of these Pages addresses a different segment of the crypto industry. Within each segment
-    (Macro, Transfers, Swaps, NFTs, etc.) you are able to filter your desired blockchains to
-    narrow/expand the comparison. By selecting a single blockchain, you can observe a deep dive
-    into that particular network.
-
-    All values for amounts, prices, and volumes are in **U.S. dollars** and the time frequency of the
-    analysis is limited to December 2023.
+    우송 대학교 에서는 IT교육센터를 운영중에 있다. 여기서는 국비 교육 과정으로 2024년 03월 06일 부터 
+    총 960시간의 교육 프로그램으로 취업연계형 응용SW 전문인력 양성을 위한 교육을 하고 있다. 
+    주요 프로그램으로는 'AI응용 SW 전문가','로봇 응용 SW 전문가' 과정을 각각 운영중에 있다. 
+    각 과정의 정원은 24명이며 480시간의 이론과 실습 교육을 하고 있으며 이후 480시간의 프로젝트 과정과 
+    인턴쉽 과정으로 구성되어 있다. 
     """
 )
 
 # Methodology
-st.subheader('Methodology')
+st.subheader('수업과정')
 st.write(
     """
-    The data for this cross-chain comparison were extracted from the [**Flipside Crypto**](https://flipsidecrypto.xyz)
-    data platform by using its **REST API**. These queries are currently set to return data for only December 2023
-    and are imported as a JSON file directly to each page. Due to the heavy computational power required to execute
-    the queries, and also the size of the raw data being too large, it was not feasible to cover data for a longer
-    period, or by downloading the data and loading it from the repository itself. Therefore, the REST API was
-    selected as the proper form of loading data for the time being. The codes for this tool are saved and accessible
-    in its [**GitHub Repository**](https://github.com/alitaslimi/cross-chain-monitoring).
+    본 수업은 Python, C, C++, Java등으로 구성 되어 있으며 chatGPT와 함께 하는 과정으로 
+    AI를 활용한 프로젝트 개발등을 교육 하고 있다. 
+    수업 방법은 오전 09:00부터 13:00까지 4시간 교육을 하고 13:00부터 14:00까지 점심시간을 갖은후 
+    14:00부터 18:00까지 오후 수업을 하도록 한다. 
+    기타 자세한 문의는 [**우송대학교IT교육센터**](https://itedu.wsu.ac.kr)를 통해 안내 하고있다.
     """
 )
 
